58b22146880f37e2bab08813e96c346f6a05edb9
starsky <starsky.zhanglinjie@gmail.com>
2024-05-20 22:44:05 +0800

    todo

--- add.js
+++ add.js
@@ -1,3 +1,10 @@
+// import fs from "node:fs";
+
 export function add(x, y) {
   return x + y;
 }
+
+// ???todo: how to combine with node native module
+// export function readfile(filename) {
+//   return fs.readFileSync(filename, { encoding: "utf-8" });
+// }
